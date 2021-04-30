cae1c5fd28d9c1410f6021d5f0143e1d6bcdccfc
apigott <aisling.pigott@colorado.edu>
2021-04-30 09:59:57 -0600

    removing old files

--- tests/rl_main.py
+++ tests/rl_main.py
@@ -14,20 +14,27 @@ from joblib import dump, load
 
 # Load environment
 climate_zone = 1
-data_path = Path("citylearn/data/Climate_Zone_"+str(climate_zone))
+data_path = Path("../citylearn/data/Climate_Zone_"+str(climate_zone))
 building_attributes = data_path / 'building_attributes.json'
 weather_file = data_path / 'weather_data.csv'
 solar_profile = data_path / 'solar_generation_1kW.csv'
-building_state_actions = 'citylearn/buildings_state_action_space.json'
+building_state_actions = '../citylearn/buildings_state_action_space.json'
 building_id = ["Building_1","Building_2","Building_3","Building_4","Building_5","Building_6","Building_7","Building_8","Building_9"]
 objective_function = ['ramping','1-load_factor','average_daily_peak','peak_demand','net_electricity_consumption','quadratic','voltage_dev']
 
-ep_period = 8760
+timestep = 4 # per hour
+
+start_month = 0 # approx
+end_month = 0.25
+ep_start = int(start_month * 30 * 24* timestep)
+ep_end = int((end_month * 30 * 24 * timestep)-1)
+ep_period = ep_end - ep_start
 
 print("Initializing the grid...")
 # Contain the lower and upper bounds of the states and actions, to be provided to the agent to normalize the variables between 0 and 1.
 # Can be obtained using observations_spaces[i].low or .high
-env = GridLearn(data_path, building_attributes, weather_file, solar_profile, building_id, 1, buildings_states_actions = building_state_actions, simulation_period = (0,ep_period), cost_function = objective_function, verbose=1, n_buildings_per_bus=1)
+env = GridLearn(data_path, building_attributes, weather_file, solar_profile, building_id, timestep, buildings_states_actions = building_state_actions, simulation_period = (ep_start, ep_end), cost_function = objective_function, verbose=1, n_buildings_per_bus=4)
+
 
 # Hyperparameters
 batch_size = 254
@@ -61,6 +68,7 @@ for e in range(n_episodes):
     print("is_deterministic", is_evaluating)
     action, coordination_vars = agents.select_action(state, deterministic=is_evaluating)
 #     print(action)
+    ts = 0
     while not done:
         next_state, reward, done, _ = env.step(action)
         action_next, coordination_vars_next = agents.select_action(next_state, deterministic=is_evaluating)
